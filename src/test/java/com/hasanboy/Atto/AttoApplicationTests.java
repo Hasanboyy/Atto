fc097e0bf8356d570c5000872933f30a1079605f
hasanboy <metinnazif77@gmail.com>
2022-05-22 01:41:02 +0500

    Initial commit

---
+++ src/test/java/com/hasanboy/Atto/AttoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasanboy.Atto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
